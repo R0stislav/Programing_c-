cd5f02b38784b4907284e40aaac696e6f3c71010
R0stislav <106510524+R0stislav@users.noreply.github.com>
2022-05-30 09:44:24 +0300

    Add files via upload

---
+++ lab02_02_15.cpp
@@ -0,0 +1,23 @@
+﻿#include <iostream>
+#include <windows.h>
+#include <math.h>
+
+using namespace std;
+
+
+int main()
+{
+	SetConsoleOutputCP(1251);
+
+	double a = 5, x;
+
+	cout << "Введіть x: ";
+	cin >> x;
+
+	cout << sqrt(x * x * x + a) << endl;
+
+
+
+	
+}
+
