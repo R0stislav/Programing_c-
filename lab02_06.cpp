3633bb5120c80e1b7f07b83ef26cd97a34410386
R0stislav <106510524+R0stislav@users.noreply.github.com>
2022-05-30 12:03:01 +0300

    Add files via upload

---
+++ lab02_06.cpp
@@ -0,0 +1,48 @@
+﻿#include <iostream>
+#include <windows.h>
+
+using namespace std;
+
+int main()
+{
+	SetConsoleOutputCP(1251);
+
+	int a, b, c;
+	
+	
+
+
+	cout << "Введіть a: ";
+	cin >> a;
+
+	cout << "Введіть b: ";
+	cin >> b;
+
+	cout << "Введіть c: ";
+	cin >> c;
+
+	if ((b + c) > (a + b) && (a + c) > b)
+	{
+		cout << (b + c) + (a + c);
+	}
+	else if ((b + c) < (a + b) && (a + c) < b)
+	{
+		cout << (a + b) + b;
+	}
+	else if ((b + c) > (a + b) && (a + c) < b)
+	{
+		cout << (b + c) + b;
+	}
+	else if ((b + c) < (a + b) && (a + c) > b)
+	{
+		cout << (a + b) + (a + c);
+	}
+
+	
+
+
+
+
+
+}
+
