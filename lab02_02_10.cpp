22a40525481a63e6c65cbaad899121dd30aee2f9
R0stislav <106510524+R0stislav@users.noreply.github.com>
2022-05-30 12:02:34 +0300

    Add files via upload

---
+++ lab02_02_10.cpp
@@ -0,0 +1,40 @@
+﻿#include <iostream>
+#include <windows.h>
+
+using namespace std;
+
+int main()
+{
+    SetConsoleOutputCP(1251);
+
+    int X, Y, Z;
+
+    cout << "Введіть X: ";
+    cin >> X;
+
+    cout << "Введіть Y: ";
+    cin >> Y;
+
+    cout << "Введіть Z: ";
+    cin >> Z;
+
+    if (Z > Y && Z > X)
+    {
+        cout << X << endl;
+        cout << Y << endl;
+        cout << Z << endl;
+    }
+    else if (X < Y && Z > X)
+    {
+        cout << X << endl;
+        cout << Y << endl;
+        cout << Z << endl;
+    }
+    else
+    {
+        cout << "X не є найменшим, Z не є найбільшим";
+    }
+
+    
+}
+
