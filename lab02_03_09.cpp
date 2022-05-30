685da1074d2d1d62a86c09b8b189d244ba3c28c4
R0stislav <106510524+R0stislav@users.noreply.github.com>
2022-05-30 09:45:15 +0300

    Add files via upload

---
+++ lab02_03_09.cpp
@@ -0,0 +1,30 @@
+﻿#include <iostream>
+#include <windows.h>
+
+using namespace std;
+
+int main()
+{
+	SetConsoleOutputCP(1251);
+
+	int x, y;
+
+	cout << "Введіть X: ";
+	cin >> x;
+
+	cout << "Введіть Y: ";
+	cin >> y;
+
+	if (y <= 1 && y >= 0 && y >= 1 * x - 1 && y >= -1 * x - 1)
+	{
+		cout << "належить";
+	}
+	else
+	{
+		cout << "не належить";
+	}
+
+
+	
+}
+
