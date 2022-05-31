3712677e0fb8b872cfbc3de45d145d19447e2721
R0stislav <106510524+R0stislav@users.noreply.github.com>
2022-05-31 14:56:05 +0300

    Add files via upload

---
+++ lab02_03_10.cpp
@@ -0,0 +1,32 @@
+﻿#include <iostream>
+#include <math.h>
+
+using namespace std;
+
+int main()
+{
+    int n, a;
+
+    cout << "input a: ";
+    cin >> a;
+
+    cout << "input n: ";
+    cin >> n;
+
+    while 
+
+    
+
+
+    
+
+    
+
+    
+
+
+
+   
+
+}
+
