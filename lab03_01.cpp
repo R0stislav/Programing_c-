2f2a754db9eaf7882495c6b7893b760358b6175f
R0stislav <106510524+R0stislav@users.noreply.github.com>
2022-06-07 21:49:44 +0300

    Add files via upload

---
+++ lab03_01.cpp
@@ -0,0 +1,31 @@
+﻿#include <iostream>
+#include <windows.h>
+
+using namespace std;
+
+
+int main()
+{
+    SetConsoleOutputCP(1251);
+
+    int a, h, s;
+
+    cout << "введіть сторону а";
+    cin >> a;
+
+    cout << "Введіть висоту h";
+    cin >> h;
+
+    int* ptr_A;
+    int* ptr_H;
+
+    ptr_A = &a;
+    ptr_H = &h;
+
+    cout << 1 / 2 * (ptr_A * ptr_H) << endl;
+    
+
+
+    
+}
+
