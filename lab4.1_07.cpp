225d4178272e11707691a76a19a07f5f3e979fe4
R0stislav <106510524+R0stislav@users.noreply.github.com>
2022-06-09 01:18:37 +0300

    Add files via upload

---
+++ lab4.1_07.cpp
@@ -0,0 +1,36 @@
+﻿#include <iostream>
+#include <windows.h>
+
+using namespace std;
+
+
+int main()
+{
+    SetConsoleOutputCP(1251);
+
+    int r, X[100], Y[100];
+    printf("Введіть розмірність масивів r<=100 --> "); scanf_s("%d", &r); srand((unsigned)time(NULL));
+    for (int i = 0; i < r; i++) {
+         X[i] = 100 - ((double)rand() / RAND_MAX) * 200; Y[i] = 100 - ((double)rand() / RAND_MAX) * 200;
+         
+            
+    }
+     printf("\nМасиви до обміну\n"); printf("No\tX[]\t\tY[]\n");
+     for (int i = 0; i < r; i++) {
+         printf("%d\t%d\t\t%d\n", i, X[i], Y[i]); 
+        
+    }
+     for (int i = 0; i < r; i++) {
+         X[i] = X[i] + Y[r - i - 1]; Y[r - i - 1] = X[i] - Y[r - i - 1]; X[i] = X[i] - Y[r - i - 1]; 
+        
+    }
+     printf("\nМасиви після обміну\n"); printf("No\tX[]\t\tY[]\n");
+     for (int i = 0; i < r; i++) {
+         printf("%d\t%d\t\t%d\n", i, X[i], Y[i]); 
+        
+    }
+
+
+
+}
+
