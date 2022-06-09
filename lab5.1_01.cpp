09aa35732f384dbde542e639423b20f3387dba37
R0stislav <106510524+R0stislav@users.noreply.github.com>
2022-06-09 16:52:57 +0300

    Add files via upload

---
+++ lab5.1_01.cpp
@@ -0,0 +1,26 @@
+﻿#include <iostream>
+#include <windows.h>
+
+using namespace std;
+
+int main()
+{
+    SetConsoleOutputCP(1251);
+
+    char str[50]; 
+    int i;
+    int n_p; 
+    int n_m; 
+
+    
+
+    n_p = 0;
+    n_m = 0;
+    for (i = 0; i < strlen(str); i++)
+    {
+        if (str[i] == '+')
+            n_p++;
+        if (str[i] == '-')
+            n_m++;
+    }
+
