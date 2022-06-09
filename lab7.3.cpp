782fbc4f773fd930b4e31be21cf694f647216d73
R0stislav <106510524+R0stislav@users.noreply.github.com>
2022-06-10 01:11:58 +0300

    Add files via upload

---
+++ lab7.3.cpp
@@ -0,0 +1,41 @@
+﻿#include <iostream>
+#include <windows.h>
+
+using namespace std;
+
+void foo()
+{
+    int a, b, c;
+
+    cout << "Введіть a: ";
+    cin >> a;
+
+    cout << "Введіть b: ";
+    cin >> b;
+
+    cout << "Введіть c: ";
+    cin >> c;
+
+    int* ptr_A;
+    int* ptr_B;
+    int* ptr_C;
+
+    ptr_A = &a;
+    ptr_B = &b;
+    ptr_C = &c;
+
+    ptr_C = ptr_C + 3;
+
+    ptr_A == ptr_C && ptr_C == ptr_A;
+
+    cout << ptr_A << endl << ptr_B << endl << ptr_C;
+}
+int main()
+{
+    SetConsoleOutputCP(1251);
+
+    foo();
+    
+    
+}
+
