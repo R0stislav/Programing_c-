3cb87a535a197549721e175712209c61e936fb9c
R0stislav <106510524+R0stislav@users.noreply.github.com>
2022-06-09 17:28:28 +0300

    Add files via upload

---
+++ lab5.2_02.cpp
@@ -0,0 +1,17 @@
+﻿#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    string a = "Мотор, Рот, Тир, Монітор";
+    istringstream ss(a);
+    string t;
+    int n = 0;
+    while (ss >> t)
+    {
+        if (n++ % 2 == 0)
+            cout << t << endl;
+    }
+}
+
