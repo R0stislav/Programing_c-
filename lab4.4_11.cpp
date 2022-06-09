442cdf440324d8aa3e26521a4e835bae8f456dab
R0stislav <106510524+R0stislav@users.noreply.github.com>
2022-06-09 15:56:07 +0300

    Add files via upload

---
+++ lab4.4_11.cpp
@@ -0,0 +1,34 @@
+﻿#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int n, i, j, matrix[40][40];
+    
+    printf("Введіть розмірність матриці-- >");
+    scanf_s("%d", &n);
+    for (i = 0; i < n; i++) {
+        for (j = 0; j < n; j++) {
+            matrix[i][j] = 100 - ((double)rand() / RAND_MAX) * 200;
+           
+            printf("matrix[%d][%d] = %d\t", i, j, matrix[i][j]);
+            
+        printf("\n");
+        
+    for (i = 0; ++i < n;) {
+        for (j = 0; j < i;) {
+            matrix[i][j] += matrix[j][i];
+            matrix[j][i] = matrix[i][j] - matrix[j][i];
+            matrix[i][j] -= matrix[j++][i];
+        }
+    }
+    for (i = 0; i < n; i++) {
+        for (j = 0; j < n; j++)
+            printf("matrix[%d][%d] = %d\t", i, j, matrix[i][j]);
+        printf("\n");
+    }
+    return 0;
+    
+}
+
