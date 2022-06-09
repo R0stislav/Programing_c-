c2431ebb544735c9a65c076f17cf21a5fb2d17ca
R0stislav <106510524+R0stislav@users.noreply.github.com>
2022-06-09 14:40:50 +0300

    Add files via upload

---
+++ lab4.2_12.cpp
@@ -0,0 +1,27 @@
+﻿#include <iostream>
+#include <cmath>
+#include <time.h>
+
+
+using namespace std;
+    int main()
+    {
+        const int n = 24, m = 10;
+        int a[n];
+        long long int b[m] = { 0 };
+        srand(time(0));
+        for (int i = 0; i < n; i++)
+            a[i] = rand() % 50 + 1;
+        for (int i = 0; i < m; i++)
+            for (int j = 0; j < n; j++)
+                b[i] += pow(a[j], double(i + 1));
+        for (int i = 0; i < n; i++)
+            cout << "a[" << i + 1 << "]=" << a[i] << " " << endl;
+        cout << endl;
+        for (int i = 0; i < m; i++)
+            cout << "b[" << i + 1 << "]=" << b[i] << " " << endl;
+        cout << endl;
+        return 0;
+    }
+
+
