e6f8f45984762fb424386f2606987b7b04c9f96f
R0stislav <106510524+R0stislav@users.noreply.github.com>
2022-06-09 18:10:18 +0300

    Add files via upload

---
+++ lab2.4_05.cpp
@@ -0,0 +1,20 @@
+﻿#include <iostream>
+#include <cmath>
+
+using namespace std;
+
+int main()
+{ 
+	double i, j;
+
+	cout << "input i: ";
+	cin >> i;
+
+	cout << "input j: ";
+	cin >> j;
+
+	cout << (100 * (100 + 1) / 2) * (50 * (50 + 1) / 2) * (i + j);
+
+}
+
+
