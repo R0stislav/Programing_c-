1796f118ad4cd1844847c90cb303624bc55c4882
R0stislav <106510524+R0stislav@users.noreply.github.com>
2022-06-09 16:53:51 +0300

    Add files via upload

---
+++ lab5.1_07.cpp
@@ -0,0 +1,39 @@
+﻿#include <iostream>
+#include <conio.h>
+#include <clocale>
+
+       int ascii_cod(char x); 
+
+       using namespace std;
+
+int main(int argc, char* argv[])
+{
+    setlocale(LC_ALL, "ukr");
+    char c;
+    cout << "         Введіть символ: ";
+    cin >> c;
+    int b = ascii_cod(c);
+    cout << " ASCII код цього символа " << c << " = " << b << endl;
+
+    cout << "____________________________________" << endl;
+
+    int ch;
+    cout << "Введить ASCII код: ";
+    cin >> ch;
+    cout << " символ:  ";
+    cout.put(ch);
+
+    getch();
+}
+
+int ascii_cod(char x)
+{
+    int a;
+    a = x;
+    return a;
+}
+
+
+    
+
+
