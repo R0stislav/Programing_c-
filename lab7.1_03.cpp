26a7bf4732d661d039b4eaea2aa2ff889485ec65
R0stislav <106510524+R0stislav@users.noreply.github.com>
2022-06-10 01:09:45 +0300

    Add files via upload

---
+++ lab7.1_03.cpp
@@ -0,0 +1,24 @@
+﻿#include <iostream>
+#include <stdio.h>
+
+int DigitSum(int n)
+{
+    int res;
+
+    if (n < 10) return n;
+
+    res = n / 10;
+    return n - res * 10 + DigitSum(res);
+}
+
+int main()
+{
+    int n;
+    printf("Vvedite chislo:  ");
+    scanf("%d", &n);
+
+    printf("\nSumma: %d\n", DigitSum(n));
+    scanf(" ");
+    
+}
+
