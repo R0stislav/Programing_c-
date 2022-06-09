2fa1b0b84b8a5c6b5ed4e448d4772667b7d7a037
R0stislav <106510524+R0stislav@users.noreply.github.com>
2022-06-10 01:10:58 +0300

    Add files via upload

---
+++ lab7.2_10.cpp
@@ -0,0 +1,24 @@
+﻿#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int n;
+    double a, p = 1.0, sum = 0.0;
+    cout << "n="; cin >> n;
+
+    cout << "Enter " << n << " elements:\n";
+    for (int i = 1; i <= n; i++)
+    {
+        cin >> a;
+        p *= -1.0 / i;
+        sum += a * p;
+    }
+
+    cout << "sum=" << sum << "\n";
+    system("pause");
+    return 0;
+}
+
+
